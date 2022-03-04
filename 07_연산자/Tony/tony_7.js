40af37598c104cbe08ba58b0addbab205be12cce
Tony <gth1123@naver.com>
2022-03-04 22:54:55 +0900

    tony : 7장

---
+++ 07_연산자/Tony/tony_7.js
@@ -0,0 +1,13 @@
+// console.log(!!1);
+// console.log(!!NaN);
+// console.log(!![]);
+// if ([]) {
+//   console.log("hello");
+// }
+
+// console.log(1 + true); // 2
+// console.log(1 + false); // 1
+// console.log(1 + null); // 1 <- null : 0
+
+console.log(2 ** 3); // 8
+console.log(Math.pow(2, 3)); // 8
