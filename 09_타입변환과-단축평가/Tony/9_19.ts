871fb332e25bcec3e15edfb0195569b8062e08de
Tony <gth1123@naver.com>
2022-03-07 22:50:03 +0900

    tony : 9장

---
+++ 09_타입변환과-단축평가/Tony/9_19.ts
@@ -0,0 +1,8 @@
+// 단축 평가
+console.log("cat" && "dog"); // dog
+console.log(false && "dog"); // false
+console.log("cat" && false); // false
+
+console.log("cat" || "dog"); // cat
+console.log(false || "dog"); // dog
+console.log("cat" || false); // cat
