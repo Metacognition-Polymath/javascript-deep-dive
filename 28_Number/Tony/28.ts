bedc2a37b4fc569c0cd72baabb2e3f799e021353
Tony <gth1123@naver.com>
2022-04-05 23:11:08 +0900

    tony : 28장 보충

--- 28_Number/Tony/28.ts
+++ 28_Number/Tony/28.ts
@@ -1,4 +1,12 @@
 {
-  const numObj = new Number(1);
-  console.log(numObj); // Number {[[PrimitiveValue]]: 10}
+  // const numObj = new Number(1);
+  // console.log(numObj); // Number {[[PrimitiveValue]]: 10}
+}
+{
+  // const result = (12345.67889).toFixed(2); // "12345.68"
+  // console.log(result);
+}
+{
+  console.log(Number.isNaN(undefined)); // false
+  console.log(isNaN(undefined)); // true
 }
